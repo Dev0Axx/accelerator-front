a3ac21e6c73a1a356a8bb9280cbdff63bd5ee1c6
Gorkavoy Andrey <andreystartprime@inbox.ru>
2025-10-24 18:24:33 +0300

    Сделал заготовку для всех страниц

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { CssBaseline, Box, Typography } from '@mui/material'
+import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from './contexts/ThemeContext'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage/HomePage'
-
-const CatchPage = () => (
-	<Box>
-		<Typography variant='h4' gutterBottom>
-			Ввод улова
-		</Typography>
-		<Typography variant='body1'>
-			Форма для ввода улова будет здесь...
-		</Typography>
-	</Box>
-)
-
-const MyCatchesPage = () => (
-	<Box>
-		<Typography variant='h4' gutterBottom>
-			Мои уловы
-		</Typography>
-		<Typography variant='body1'>
-			Таблица с историей уловов будет здесь...
-		</Typography>
-	</Box>
-)
-
-const OverviewPage = () => (
-	<Box>
-		<Typography variant='h4' gutterBottom>
-			Обзор уловов
-		</Typography>
-		<Typography variant='body1'>
-			Общая статистика по всем уловам будет здесь...
-		</Typography>
-	</Box>
-)
-
-const QuotasPage = () => (
-	<Box>
-		<Typography variant='h4' gutterBottom>
-			Управление квотами
-		</Typography>
-		<Typography variant='body1'>Настройка квот будет здесь...</Typography>
-	</Box>
-)
+import CatchPage from './pages/CatchPage/CatchPage'
+import OverviewPage from './pages/OverviewPage/OverviewPage'
+import MyCatchesPage from './pages/MyCatchesPage/MyCatchesPage'
+import QuotasPage from './pages/QuotasPage/QuotasPage'
+import LoginPage from './pages/LoginPage/LoginPage'
+import RegisterPage from './pages/RegisterPage/RegisterPage'
 
 function App() {
 	return (
@@ -58,6 +22,8 @@ function App() {
 						<Route path='/my-catches' element={<MyCatchesPage />} />
 						<Route path='/overview' element={<OverviewPage />} />
 						<Route path='/quotas' element={<QuotasPage />} />
+						<Route path='/login' element={<LoginPage />} />
+						<Route path='/register' element={<RegisterPage />} />
 					</Routes>
 				</MainLayout>
 			</Router>
